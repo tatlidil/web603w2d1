c64b6ef0508aa3034e8c10c8c39e02668ed4bef8
unknown <tatlidilonur0@gmail.com>
2024-05-13 19:37:03 -0400

    all demos done

--- demo3.js
+++ demo3.js
@@ -1,27 +1,36 @@
-  class NameForm extends React.Component {
+
+class NameForm extends React.Component {
     constructor(props) {
-      super(props);
-      
+        super(props);
+        this.state = { value: '' };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
-  
+
+    handleChange(event) {
+        this.setState({ value: event.target.value });
+    }
+
     handleSubmit(event) {
-      
+        alert('A name was submitted: ' + this.state.value);
+        event.preventDefault();
     }
-  
+
     render() {
-      return (
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Name:
-            <input type="text"  />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-      );
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Name:
+                    <input type="text" value={this.state.value} onChange={this.handleChange} />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
+        );
     }
-  }
-  
-  ReactDOM.render(
+}
+
+ReactDOM.render(
     <NameForm />,
     document.getElementById('root')
-  );
+);
--- demo4.js
+++ demo4.js
@@ -1,17 +1,24 @@
-class FileInput extends React.Component {
-    constructor(props) {
-    
+class NameForm extends React.Component {
+  constructor(props) {
       super(props);
-      
-    }
-    handleSubmit(event) {
-      
-    }
+      this.fileInput = React.createRef();
+
+      this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+      event.preventDefault();
+      alert(`Selected file - ${this.fileInput.current.files[0].name}`);
+  }
   
     render() {
    
       return (
         <form onSubmit={this.handleSubmit}>
+          <label>
+              Upload file:
+            <input type="file" ref={this.fileInput} />
+          </label>
           
           <br />
           <button type="submit">Submit</button>
